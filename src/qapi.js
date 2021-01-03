e002adf29e95efc9050ee056590d59135a33e160
Ishan666678 <74366197+Ishan666678@users.noreply.github.com>
2021-01-03 20:33:37 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5feef19c0fa39a001e3f0926",
+				"5feef19c0fa39a001e3f0928"
+			]
+		},
+		"5feef19c0fa39a001e3f0926": {
+			"id": "5feef19c0fa39a001e3f0926",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5feef19c0fa39a001e3f0928": {
+			"id": "5feef19c0fa39a001e3f0928",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
